65ef369b9007c77881fd38ce0b2b4f73c2c600ff
guo.xu <xuguo@outlook.it>
2021-01-24 22:09:51 +0800

    chore: 添加eslint配置创建eslint preset包

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  env: {
+    browser: true,
+    commonjs: true,
+    node: true,
+    es6: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'no-console': 'off',
+    strict: ['error', 'global'],
+    curly: 'warn',
+  },
+};
